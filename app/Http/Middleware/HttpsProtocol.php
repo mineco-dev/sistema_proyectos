c8cb4f01769725a5da48b9d6089db9a278ca0547
juanjo5516 <juanjosedelaguilalopez@gmail.com>
2022-03-10 10:56:55 -0600

    Cambios en middleware para forzar a https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,6 +16,9 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
+        if (!$request->secure() && app()->environment('production')) {
+            return redirect()->secure($request->getRequestUri());
+        }
         return $next($request);
     }
 }
