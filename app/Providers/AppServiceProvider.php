6f7d1fc3ae5e2246ca58ee1c70a016ae484783db
juanjo5516 <juanjosedelaguilalopez@gmail.com>
2022-03-10 12:06:01 -0600

    Cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV') === 'productions') {
             URL::forceScheme('https');
             URL::forceRootUrl(Config::get('app.url'));
         }
